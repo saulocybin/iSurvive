3e277b01e0a06bbe0ebdf0afe33d4de6f60f8e36
powerhaus <saulobrart@outlook.com>
2020-04-19 17:59:41 +0100

    Basic working functional implementation of OrbitDB implemented

---
+++ index.js
@@ -0,0 +1,18 @@
+const IPFS = require("ipfs");
+const OrbitDB = require("orbit-db");
+
+(async (IPFS, OrbitDB) => {
+  const ipfs = await IPFS.create({});
+  const orbitdb = await OrbitDB.createInstance(ipfs);
+  //console.log(orbitdb);
+
+  const db = await orbitdb.keyvalue('first-database');
+  //console.log(db);
+
+  const address = db.address;
+  console.log(address);
+
+  
+
+  
+})(IPFS, OrbitDB);
