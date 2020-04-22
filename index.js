c87fabe48e1901821a17fdd0ec2b4ddc72f79c00
powerhaus <saulobrart@outlook.com>
2020-04-22 12:21:43 +0100

    added access control

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 const IPFS = require("ipfs");
 const OrbitDB = require("orbit-db");
+const Identities = require('orbit-db-identity-provider');
 
 (async (IPFS, OrbitDB) => {
+  //init ipfs
   const ipfs = await IPFS.create({});
-  const orbitdb = await OrbitDB.createInstance(ipfs);
-  //console.log(orbitdb);
 
+  //create identity
+  const options = { id: 'local-saulocybin' };
+  const identity = await Identities.createIdentity(options);
+
+  //init orbitdb
+  const orbitdb = await OrbitDB.createInstance(ipfs, {identity: identity});
+  
+  //create database
   const db = await orbitdb.keyvalue('first-database');
-  //console.log(db);
+  
+  console.log("PUBLIC KEY IDENTITY:");
+  console.log(db.identity.publicKey);
 
+  console.log("DB ADDRESS:");
   const address = db.address;
   console.log(address);
 
   await db.put('name', 'hello')
   console.log(db);
 
-  
+
 })(IPFS, OrbitDB);
